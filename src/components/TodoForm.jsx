4c139c8298b50e8e74dbfc93ee7f06e839062736
LucasAndersonnnn <orwanthre@outlook.com>
2026-02-19 00:51:05 -0300

    Css, setvalue/setcategory para o gerenciamento do formulário

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import { useState } from 'react';
 
 const TodoForm = () => {
+    const [value, setValue] = useState("")
+    const [category, setCategory] = useState("")
+
+    const handleSumit = (e) => {
+        e.preventDefault();
+        if(!value || !category) return;
+
+        setValue("")
+        setCategory("")
+    }
+
   return (
   <div className="todo-form">
         <h2>Criar Tarefas</h2>
-        <form>
-            <input type="text" placeholder='Nome da Tarefa'/>
-            <select>
+        <form onSubmit ={handleSumit}>
+            <input type="text" placeholder='Nome da Tarefa' value={value} onChange={(e) => setValue(e.target.value)}/>
 
-                <option value="Trabalho">Selecione uma categoria</option>
-                <option value="Pessoal">Selecione uma categoria</option>
-                <option value="Estudos">Selecione uma categoria</option>
+            <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                <option value="">Escolha uma opção</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Pessoal">Pessoal</option>
+                <option value="Estudos">Estudos</option>
 
             </select>
             <button type="submit">Criar Tarefa</button>
